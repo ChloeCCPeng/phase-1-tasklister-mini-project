af835c1bffa6ff86d66a1c26c889fdc2f6ce6c92
Chloe <chloepeng331@icloud.com>
2022-02-19 19:47:21 -0500

    assignment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const form = document.querySelector('#create-task-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const task = document.querySelector('#new-task-description').value
+  const tasks = document.querySelector('#tasks')
+  tasks.textContent = task
+  })
